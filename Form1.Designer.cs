daab764254c516251bdfc628ecf5da3d56292b4e
PinRong1014 <113153001@gas.fgu.edu.tw>
2025-03-10 15:27:32 +0800

    第一次更新
    公分到公尺的單位轉換

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -51,6 +51,7 @@
             this.txtCM.Name = "txtCM";
             this.txtCM.Size = new System.Drawing.Size(108, 25);
             this.txtCM.TabIndex = 0;
+            this.txtCM.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtCM_KeyUp);
             // 
             // label1
             // 
@@ -71,6 +72,7 @@
             this.btnAllClear.TabIndex = 2;
             this.btnAllClear.Text = "清除";
             this.btnAllClear.UseVisualStyleBackColor = true;
+            this.btnAllClear.Click += new System.EventHandler(this.btnAllClear_Click);
             // 
             // label2
             // 
